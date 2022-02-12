022b8517708714242669bcee1f4e06d9548a1c72
kahenteikou <kokkiemouse@gmail.com>
2022-02-12 19:56:57 +0900

    fx

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
     version='0.0.1',
     license='MIT',
     description='mia_libraries_cy',
-
     author='fukukoussjouhou',
     author_email='umekoujouhouhan@gmail.com',
     url='None.com',
