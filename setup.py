395c7dc58b7f4b10f9c4da05476cb1b162ab551d
kahenteikou <kokkiemouse@gmail.com>
2022-02-13 10:11:11 +0900

    fixed

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ from Cython.Build import cythonize
 from setuptools import setup, find_packages
 from setuptools.extension import Extension
 
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
 setup(
     name='mia_libraries_cy',
-    version='0.0.1',
+    version='0.0.0.1',
     license='MIT',
     description='mia_libraries_cy',
     author='fukukoussjouhou',
@@ -12,6 +15,8 @@ setup(
     url='None.com',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
+    setup_requires=["Cython"],
+    install_requires=_requires_from_file('requirements.txt'),
     ext_modules=cythonize([
         Extension(
             "MIA_Libraries_CY.KyokoLoggingkun",
